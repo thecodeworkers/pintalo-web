26486947290fa43e8474a2ebb8dce45ed24a405f
Victor Caceres <caceres.vc95@gmail.com>
2021-04-05 11:35:26 -0400

    Add redux 🔥

---
+++ src/store/index.ts
@@ -0,0 +1,56 @@
+import { createStore, applyMiddleware } from 'redux'
+import { HYDRATE, createWrapper } from 'next-redux-wrapper'
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+import thunkMiddleware from 'redux-thunk'
+import reducers from './reducers'
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const bindMiddleware = (middleware) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension')
+    return composeWithDevTools(applyMiddleware(...middleware))
+  }
+
+  return applyMiddleware(...middleware)
+}
+
+const reducer = (state, action) => {
+  if (action.type === HYDRATE) return { ...state, ...action.payload }
+  if (action.type == 'persist/REHYDRATE') action.payload = { ...action.payload, ...state }
+
+  return reducers(state, action)
+}
+
+const makeStore: any = ({ isServer }) => {
+  const createStoreHook = currentReducer => createStore(currentReducer, bindMiddleware([thunkMiddleware]))
+  if (isServer) return createStoreHook(reducer)
+
+  const { persistStore, persistReducer } = require('redux-persist')
+  const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
+
+  const persistConfig = {
+    key: 'pintaloRoot',
+    storage
+  }
+
+  const persistedReducer = persistReducer(persistConfig, reducer)
+  const store = createStoreHook(persistedReducer)
+  store['__persistor'] = persistStore(store, { manualPersist: true })
+
+  return store
+}
+
+export const wrapper = createWrapper(makeStore)
