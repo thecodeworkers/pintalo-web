2368d0e121e8d4a73781f42b6dcc2734a3872922
Victor Caceres <caceres.vc95@gmail.com>
2021-04-26 22:52:59 -0400

    Defining areas

--- src/components/Color/index.tsx
+++ src/components/Color/index.tsx
@@ -8,17 +8,17 @@ function Color() {
         <ColorBackground color="#3772ff" />
       </div>
       <div className={styles._descriptionMobileContainer}>
-        <p>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
+        <p className={styles._description}>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
       </div>
       <div className={styles._contentContainer}>
-        <div style={{ height: '80%', backgroundColor: 'purple', display: 'flex', alignItems: 'center' }}>
-          <div style={{ height: '45%', backgroundColor: 'red', width: '100%' }}>
-            <div style={{ fontSize: 80, marginBottom: 10 }}>
+        <div className={styles._optionsContainer}>
+          <div className={styles._optionsContainer_children}>
+            <div className={styles._colorTitle} style={{ height: '25%' }}>
               Blue
             </div>
-            <div style={{ display: 'flex', marginBottom: 30, backgroundColor: 'green' }}>
+            <div style={{ display: 'flex', backgroundColor: 'green', height: '50%' }}>
               <div style={{ backgroundColor: 'blue', marginRight: 30, width: '60%' }}>
-                <div style={{ marginBottom: 12 }}>
+                <div>
                   <GeneralButton backgroundColor="#262833" textColor="#FFFFFF">
                     Añadir al carrito
                   </GeneralButton>
@@ -28,7 +28,7 @@ function Color() {
                 </GeneralButton>
               </div>
               <div style={{ width: '30%' }}>
-                <div style={{ marginBottom: 12 }}>
+                <div>
                   <GeneralButton backgroundColor="#262833" textColor="#FFFFFF">
                     <img src="images/icons/calculator.svg" alt="search" width="16px" />
                   </GeneralButton>
@@ -38,8 +38,8 @@ function Color() {
                 </GeneralButton>
               </div>
             </div>
-            <div className={styles._descriptionContainer}>
-              <p>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
+            <div className={styles._descriptionContainer} style={{ height: '25%' }}>
+              <p className={styles._description}>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
             </div>
           </div>
         </div>
