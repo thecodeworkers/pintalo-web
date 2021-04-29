ae2bf2d56a794c6110ec0b98c3dc36848bc39618
Victor Caceres <caceres.vc95@gmail.com>
2021-04-29 16:21:08 -0400

    Zip class and add style jsx input size

--- src/components/Color/index.tsx
+++ src/components/Color/index.tsx
@@ -13,74 +13,82 @@ const Color = () => {
   }, [])
 
   return (
-    <div className={styles._container}>
-      <div className={styles._imageContainer}>
-        <ColorBackground color="#3772ff" />
-      </div>
-      <div className={styles._descriptionMobileContainer}>
-        <p className={styles._description}>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
-      </div>
-      <div className={styles._contentContainer}>
-        <div className={styles._optionsContainer}>
-          <div className={styles._optionsContainer_children}>
-            <div className={styles._colorTitle}>
-              Blue
-            </div>
-            <div className={styles._colorOptions}>
-              <div className={styles._colorOptions_row}>
-                <div className={styles._lengthArea}>
-                  <div className={styles._lengthSelect}>
-                    <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
-                      Tamaño
-                    </GeneralButton>
-                  </div>
-                </div>
-                <div className={styles._calculatorMobileArea}>
-                  <div className={styles._calculatorButton}>
-                    <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
-                      <img src="images/icons/calculator.svg" alt="search" width="16px" />
-                    </GeneralButton>
-                  </div>
-                </div>
+    <>
+      <div className={styles._container}>
+        <div className={styles._imageContainer}>
+          <ColorBackground color="#3772ff" />
+        </div>
+        <div className={styles._descriptionMobileContainer}>
+          <p className={styles._description}>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
+        </div>
+        <div className={styles._contentContainer}>
+          <div className={styles._optionsContainer}>
+            <div className={styles._optionsContainer_children}>
+              <div className={styles._colorTitle}>
+                Blue
               </div>
-              <p className={styles._quantityTitle}>Cantidad</p>
-              <div className={styles._colorOptions_row}>
-                <div className={styles._quantityArea}>
-                  <div className={styles._quantityInputContainer}>
-                    <input type="text" className={styles._inputQuantity} style={{ height: retina ? '3.3rem' : '2.5rem' }} />
-                  </div>
-                  <div className={styles._arrowContainer}>
-                    <div className={styles._arrowContainer_left}>
-                      <CounterButton direction="_left" size={retina ? '3.3rem' : '2.5rem'} />
+              <div className={styles._colorOptions}>
+                <div className={styles._colorOptions_row}>
+                  <div className={styles._lengthArea}>
+                    <div className={styles._lengthSelect}>
+                      <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
+                        Tamaño
+                      </GeneralButton>
                     </div>
-                    <div className={styles._arrowContainer_right}>
-                      <CounterButton direction="_right" size={retina ? '3.3rem' : '2.5rem'} />
+                  </div>
+                  <div className={styles._calculatorMobileArea}>
+                    <div className={styles._calculatorButton}>
+                      <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
+                        <img src="images/icons/calculator.svg" alt="search" width="16px" />
+                      </GeneralButton>
                     </div>
                   </div>
                 </div>
-                <div className={styles._calculatorArea}>
-                  <div className={styles._calculatorButton}>
-                    <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
-                      <img src="images/icons/calculator.svg" alt="search" width="16px" />
-                    </GeneralButton>
+                <p className={styles._quantityTitle}>Cantidad</p>
+                <div className={styles._colorOptions_row}>
+                  <div className={styles._quantityArea}>
+                    <div className={styles._quantityInputContainer}>
+                      <input type="text" className={`${styles._inputQuantity} _inputSize`} />
+                    </div>
+                    <div className={styles._arrowContainer}>
+                      <div className={styles._arrowContainer_left}>
+                        <CounterButton direction="_left" size={retina ? '3.3rem' : '2.5rem'} />
+                      </div>
+                      <div className={styles._arrowContainer_right}>
+                        <CounterButton direction="_right" size={retina ? '3.3rem' : '2.5rem'} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles._calculatorArea}>
+                    <div className={styles._calculatorButton}>
+                      <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
+                        <img src="images/icons/calculator.svg" alt="search" width="16px" />
+                      </GeneralButton>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className={styles._descriptionContainer}>
-              <p className={styles._description}>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
+              <div className={styles._descriptionContainer}>
+                <p className={styles._description}>Este tono, amable y nada agresivo a la vista, es “un potenciador de armonía, serenidad y creatividad” según los psicólogos, y que muy pronto podremos ver presente en todo tipo de disciplinas, desde moda, arte y diseño, hasta hogar y tecnología.</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles._buttonContainer}>
-          <div className={styles._buttonBox}>
-            <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
-              Añadir al carrito
-            </GeneralButton>
+          <div className={styles._buttonContainer}>
+            <div className={styles._buttonBox}>
+              <GeneralButton backgroundColor="#262833" textColor="#FFFFFF" large={retina ? '3.3rem' : '2.5rem'}>
+                Añadir al carrito
+              </GeneralButton>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <style jsx>{`
+        ._inputSize {
+          height: ${retina ? '3.3rem' : '2.5rem'};
+        }
+      `}</style>
+    </>
   )
 }
 
