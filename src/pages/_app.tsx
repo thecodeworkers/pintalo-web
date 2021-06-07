05beefa325b0751eb1cef934a30877a064ed20b3
Victor Caceres <caceres.vc95@gmail.com>
2021-06-07 12:05:09 -0400

    Add progress loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { FC, useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { useStore } from 'react-redux'
 import { Layout } from '@components'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
 import wrapper from '@store'
 import Head from 'next/head'
 import '@styles/globals.scss'
@@ -12,6 +14,16 @@ const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
   useEffect(() => {
     store.__persistor.persist()
   }, [])
+
+  const progress = new ProgressBar({
+    size: 5,
+    color: '#FDCA40',
+    delay: 100,
+  })
+
+  Router.events.on('routeChangeStart', progress.start);
+  Router.events.on('routeChangeComplete', progress.finish);
+  Router.events.on('routeChangeError', progress.finish);
 
   return (
     <>
