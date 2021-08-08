bff5aad94e95eda51def07f47f1daa16478e4668
Victor Caceres <caceres.vc95@gmail.com>
2021-08-08 13:32:44 -0400

    Add payment methods layout

--- src/components/Checkout/elements/RightSection/elements/PaymentMethods/index.tsx
+++ src/components/Checkout/elements/RightSection/elements/PaymentMethods/index.tsx
@@ -47,63 +47,79 @@ const RenderMethod = ({ value }) => {
   switch (value) {
     case 'zelle':
       return (
-        <>
+        <div className={styles._textContainer}>
           <p>Zelle</p>
-          <div>
+          <div className={styles._textQuote}>
             <p>Banco</p>
             <p>Arianna Perez</p>
             <p>arianna.plp@gmail.com</p>
           </div>
-        </>
+        </div>
       )
 
     case 'mobilePayment':
       return (
         <>
-          <p>Pago Movil</p>
-          <div>
-            <p>Banplus</p>
-            <p>J409000303</p>
-            <p>0414-0180382</p>
+          <div className={styles._textContainer}>
+            <p>Pago Movil</p>
+            <div className={styles._textQuote}>
+              <p>Banplus</p>
+              <p>J409000303</p>
+              <p>0414-0180382</p>
+            </div>
           </div>
+          <form className={styles._formContainer}>
+            <div className={styles._formItem}>
+              <label htmlFor="Nombre">Nombre completo</label>
+              <input placeholder='Nombre' className={styles._input} />
+            </div>
+            <div className={styles._formItem}>
+              <label htmlFor="Nombre">Número de comprobante</label>
+              <input placeholder='Nombre' className={styles._input} />
+            </div>
+            <div className={styles._formItem}>
+              <label htmlFor="Nombre">Banco proveniente</label>
+              <input placeholder='Nombre' className={styles._input} />
+            </div>
+          </form>
         </>
       )
 
     case 'transfer':
       return (
-        <>
+        <div className={styles._textContainer}>
           <p>Tranferencia</p>
-          <div>
+          <div className={styles._textQuote}>
             <p>Píntalo</p>
             <p>J000000000</p>
             <p>Banco</p>
             <p>0000 0000000000000000</p>
             <p>email@gmail.com</p>
           </div>
-        </>
+        </div>
       )
 
     case 'venmo':
       return (
-        <>
+        <div className={styles._textContainer}>
           <p>Venmo</p>
-          <div>
+          <div className={styles._textQuote}>
             <p>Arianna Perez</p>
             <p>arianna.plp@gmail.com</p>
           </div>
-        </>
+        </div>
       )
 
     case 'cash':
       return (
-        <>
+        <div className={styles._textContainer}>
           <p>Efectivo</p>
           <p>En caso de no disponer del monto exacto en efectivo, te ofrecemos las siguientes opciones:</p>
-          <div>
+          <div className={styles._textQuote}>
             <p>1- Recibir vuelto en efectivo, sujeto a nuestra disponibilidad.</p>
             <p>2- Completar el restante de tu pago en Bolívares a la tasa del día</p>
           </div>
-        </>
+        </div>
       )
 
     case 'tdc':
@@ -146,7 +162,7 @@ const PaymentMethods = () => {
       </div>
       <div className={styles._informationContainer}>
         <div className={styles._receiptInfoContainer}>
-          <RenderMethod value="cash" />
+          <RenderMethod value="mobilePayment" />
         </div>
         <div className={styles._receiptImgContainer}>
 
