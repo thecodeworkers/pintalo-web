c84e2bce43316920813225c7da195d04a61262aa
Victor Caceres <caceres.vc95@gmail.com>
2021-08-09 21:33:20 -0400

    Payment method desktop layout

--- src/components/Checkout/elements/RightSection/elements/PaymentMethods/index.tsx
+++ src/components/Checkout/elements/RightSection/elements/PaymentMethods/index.tsx
@@ -1,4 +1,6 @@
 import styles from './styles.module.scss'
+import Methods from './Methods'
+import { useState } from 'react'
 
 const paymentMethods = [
   {
@@ -18,10 +20,12 @@ const paymentMethods = [
     value: 'venmo'
   },
   {
-    title: 'Efectivo euro/cash'
+    title: 'Efectivo euro/cash',
+    value: 'cash'
   },
   {
-    title: 'TDC'
+    title: 'TDC',
+    value: 'tdc'
   }
 ]
 
@@ -43,96 +47,9 @@ const logos = [
   }
 ]
 
-const RenderMethod = ({ value }) => {
-  switch (value) {
-    case 'zelle':
-      return (
-        <div className={styles._textContainer}>
-          <p>Zelle</p>
-          <div className={styles._textQuote}>
-            <p>Banco</p>
-            <p>Arianna Perez</p>
-            <p>arianna.plp@gmail.com</p>
-          </div>
-        </div>
-      )
-
-    case 'mobilePayment':
-      return (
-        <>
-          <div className={styles._textContainer}>
-            <p>Pago Movil</p>
-            <div className={styles._textQuote}>
-              <p>Banplus</p>
-              <p>J409000303</p>
-              <p>0414-0180382</p>
-            </div>
-          </div>
-          <form className={styles._formContainer}>
-            <div className={styles._formItem}>
-              <label htmlFor="Nombre">Nombre completo</label>
-              <input placeholder='Nombre' className={styles._input} />
-            </div>
-            <div className={styles._formItem}>
-              <label htmlFor="Nombre">Número de comprobante</label>
-              <input placeholder='Nombre' className={styles._input} />
-            </div>
-            <div className={styles._formItem}>
-              <label htmlFor="Nombre">Banco proveniente</label>
-              <input placeholder='Nombre' className={styles._input} />
-            </div>
-          </form>
-        </>
-      )
-
-    case 'transfer':
-      return (
-        <div className={styles._textContainer}>
-          <p>Tranferencia</p>
-          <div className={styles._textQuote}>
-            <p>Píntalo</p>
-            <p>J000000000</p>
-            <p>Banco</p>
-            <p>0000 0000000000000000</p>
-            <p>email@gmail.com</p>
-          </div>
-        </div>
-      )
-
-    case 'venmo':
-      return (
-        <div className={styles._textContainer}>
-          <p>Venmo</p>
-          <div className={styles._textQuote}>
-            <p>Arianna Perez</p>
-            <p>arianna.plp@gmail.com</p>
-          </div>
-        </div>
-      )
-
-    case 'cash':
-      return (
-        <div className={styles._textContainer}>
-          <p>Efectivo</p>
-          <p>En caso de no disponer del monto exacto en efectivo, te ofrecemos las siguientes opciones:</p>
-          <div className={styles._textQuote}>
-            <p>1- Recibir vuelto en efectivo, sujeto a nuestra disponibilidad.</p>
-            <p>2- Completar el restante de tu pago en Bolívares a la tasa del día</p>
-          </div>
-        </div>
-      )
-
-    case 'tdc':
-      return (
-        <div></div>
-      )
-
-    default:
-      return null
-  }
-}
-
 const PaymentMethods = () => {
+  const [option, setOption] = useState('zelle')
+
   return (
     <div className={styles._container}>
       <div className={styles._methodsContainer}>
@@ -151,10 +68,16 @@ const PaymentMethods = () => {
                 <input
                   type="checkbox"
                   className={styles._checkboxActive}
-                  onChange={() => {}}
-                  checked={true}
+                  onChange={(e) => setOption(e.target.value)}
+                  checked={option == method.value ? true : false}
+                  value={method.value}
                 />
-                <p>{method.title}</p>
+                <p className="_methodTitle">{method.title}</p>
+                <style jsx>{`
+                  ._methodTitle {
+                    color: ${option == method.value ? '#262833' : '#9B9B9B'};
+                  }
+                `}</style>
               </div>
             ))
           }
@@ -162,7 +85,7 @@ const PaymentMethods = () => {
       </div>
       <div className={styles._informationContainer}>
         <div className={styles._receiptInfoContainer}>
-          <RenderMethod value="mobilePayment" />
+          <Methods value={option} />
         </div>
         <div className={styles._receiptImgContainer}>
 
